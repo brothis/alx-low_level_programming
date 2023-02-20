803a3b90d2d6e850adaec25e79d901efcb958e83
brothis <jkiama140@gamil.com>
2023-02-20 00:40:01 -0800

    corrections

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
  * main - puts function
- * Return: 0(pass)
+ * Return: 0 (pass)
  */
+int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like a multilingual puzzle");
 Return (0);
 }
