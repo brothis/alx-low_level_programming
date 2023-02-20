911b317aa298343c881d205fecfb444d11049da2
brothis <jkiama140@gamil.com>
2023-02-20 11:19:44 -0800

    highway

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+include < stdio.h >
+/**
+ * main - computer it is compiled and run on.
+ * Return: (0)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+printf("Sixe of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+}
