bd190257ddbb51845e9091a1c23cae6ab0c92bd9
brothis <jkiama140@gamil.com>
2023-02-20 11:33:59 -0800

    asdasdread

--- 5-printf.c
+++ 5-printf.c
@@ -1,10 +1,10 @@
-include < stdio.h >
+include <stdio.h>
 /**
  * main - Write a C program that prints exactly
  * Return: 0 (pass)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,")
-	return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
 }
