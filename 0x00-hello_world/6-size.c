d756f8a9eb8202c87d83eccc176bc9998c609053
brothis <jkiama140@gamil.com>
2023-02-20 12:38:20 -0800

    gid

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,11 +5,10 @@ include < stdio.h >
  */
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float f;
-
-printf("Sixe of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an in: %zu byte(s)\n", sizeof(int));
+	printf("Size of long int: %zu byte(S)\n", sizeof(long int));
+	printf("Size of long long int: %zu byte(s)\n", sizeof(long long int))
+	printf("Size of float: %zu byte(s)\n", sizeof(float));
+	return (0);
 }
