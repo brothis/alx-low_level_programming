302ecf741f7960287f9942670c37a96bdbbe5616
brothis <jkiama140@gamil.com>
2023-02-20 11:28:20 -0800

    Kenya

---
+++ 5-printf.c
@@ -0,0 +1,10 @@
+include < stdio.h >
+/**
+ * main - Write a C program that prints exactly
+ * Return: (0)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,")
+	return (0);
+}
