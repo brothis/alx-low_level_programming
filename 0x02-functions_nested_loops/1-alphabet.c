696bb20de0a5a08c4cbe86d4d59988fd99ea83d0
brothis <jkiama140@gamil.com>
2023-02-21 14:28:15 -0800

    2-alphabet_x10

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,26 @@
+
+#include "main.h"
+
+
+	/**
+	 * print_alphabet - print all alphabet in lowercase
+	 * Return: 0
+	 * @void: jeff
+	 */
+
+
+	int print_alphabet(void)
+	{
+		char letter;
+
+
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
+
+
+		_putchar('\n');
+	}
+
+
